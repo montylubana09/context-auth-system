b22ffe744c9ba3a98a1e588420d19887aaae1039
montylubana09 <montylubana51@gmail.com>
2025-10-15 01:04:38 -0400

    package json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig: import('next').NextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose'],
+  },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  webpack: (config: { experiments: any; }) => {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
